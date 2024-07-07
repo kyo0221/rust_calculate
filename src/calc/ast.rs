ed1459d007fcb186a84b06fbf3c962827245999d
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2024-07-07 23:02:17 +0900

    ✨feat: testの追加

--- src/calc/ast.rs
+++ src/calc/ast.rs
@@ -1,2 +1,20 @@
 /// 定数を表す
-pub struct ConstantVal(pub i32)
+pub struct ConstantVal(i32);
+
+impl ConstantVal {
+    pub fn new(val: i32) -> ConstantVal {
+        ConstantVal(val)
+    }
+
+    pub fn get(&self) -> i32 {
+        self.0
+    }
+}
+
+/// テストの実装
+#[test]
+fn Constant_val_test() {
+    let expect = 55;
+    let constant_val = ConstantVal::new(expect);
+    assert_eq!(constant_val.get(), expect);
+}
