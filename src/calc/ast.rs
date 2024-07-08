6291dba4563113c00bfdff42406e8e0be58965f8
kyo0221 <s21c1135sc@s.chibakoudai.jp>
2024-07-08 18:44:16 +0900

    🐛fix: バグ修正

--- src/calc/ast.rs
+++ src/calc/ast.rs
@@ -1,4 +1,5 @@
 ///任意の式を表す
+#[derive(Debug, PartialEq)]
 pub enum Expr {
     ConstantVal(ConstantVal),
     BinaryOp(Box<BinaryOp>),
@@ -15,6 +16,7 @@ impl Expr {
 }
 
 /// 定数を表す
+#[derive(Debug, PartialEq)]
 pub struct ConstantVal(i32);
 
 impl ConstantVal {
@@ -38,17 +40,19 @@ fn Constant_val_test() {
 }
 
 ///演算子種別
-pub enum Opkind {
+#[derive(Debug, PartialEq)]
+pub enum OpKind {
     Add,
     Sub,
     Mul,
     Div,
 }
 
-///二項円座因子を表す
+///二項演算子を表す
+#[derive(Debug, PartialEq)]
 pub struct BinaryOp {
     //適応する演算子種別
-    op_kind: Opkind,
+    op_kind: OpKind,
     //演算子の左にある式
     left_expr: Expr,
     //演算子の右にある式
@@ -57,19 +61,19 @@ pub struct BinaryOp {
 
 impl BinaryOp {
     ///BinaryOpを生成する
-    pub fn new(left_expr: Expr, right_expr: Expr) -> BinaryOp {
-        BinaryOp { left_expr, right_expr }
+    pub fn new(op_kind: OpKind, left_expr: Expr, right_expr: Expr) -> BinaryOp {
+        BinaryOp { op_kind, left_expr, right_expr }
     }
 
     ///二項演算式を評価する
     pub fn eval(&self) -> i32 {
         let right = self.right_expr.eval();
         let left = self.left_expr.eval();
-        mathch self.op_kind {
-            Opkind::Add => left + right,
-            Opkind::Sub => left - right,
-            Opkind::Mul => left * right,
-            Opkind::Div => left / right
+        match self.op_kind {
+            OpKind::Add => left + right,
+            OpKind::Sub => left - right,
+            OpKind::Mul => left * right,
+            OpKind::Div => left / right
         }
     }
 }
@@ -78,11 +82,12 @@ impl BinaryOp {
 fn binary_op_test() {
     //13+(5+1)の式を生成
     let binary_op = BinaryOp::new(
-        Opkind::Mul,
+        OpKind::Mul,
         Expr::ConstantVal(ConstantVal::new(13)),
         Expr::BinaryOp(
             Box::new(
                 BinaryOp::new(
+                    OpKind::Add,
                     Expr::ConstantVal(ConstantVal::new(5)),
                     Expr::ConstantVal(ConstantVal::new(1)),
                 )
